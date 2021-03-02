308b9675da8349609a234ca729af6e685d6f68bf
Sollmyrr <karbos2004@gmail.com>
2021-03-02 15:56:51 +0100

    Ray cast

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Camera cam;
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButton(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("We hit " + hit.collider.name + " " + hit.point);
+
+                // Move our player to position where we clicked(hit).
+                
+                // Stop focusing any object.
+            }
+
+        }
     }
 }
