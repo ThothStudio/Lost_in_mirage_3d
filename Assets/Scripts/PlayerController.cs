5910dd92aa1689b7a8cfa81680609e264c4d10e5
Sollmyrr <karbos2004@gmail.com>
2021-03-04 16:50:52 +0100

    Player Controller i Pathfinding.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,30 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour
 {
-    Camera cam;
+    private Camera cam;
+    private PlayerMotor motor;
+    
+    public LayerMask movementMask;
+
     void Start()
     {
         cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        // Moving
+        if (Input.GetMouseButton(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if(Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
             {
                 Debug.Log("We hit " + hit.collider.name + " " + hit.point);
 
                 // Move our player to position where we clicked(hit).
-                
+                motor.MoveToPoint(hit.point);
+
                 // Stop focusing any object.
             }
+        }
 
+        // Action
+        if (Input.GetMouseButton(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                // Chcek if we hit an interactable.
+                // If we did set it as our focus.
+            }
         }
     }
 }
